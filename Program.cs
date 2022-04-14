d247a790cc1a3155904eb71fe6ede7534e032288
Robert Brunetto <robert@brunetto.cz>
2022-04-14 15:10:13 +0200

    Version 1.0.0.0: Prints usage, ignores recycle bin, System Volume Information, warns about files starting with .

--- Program.cs
+++ Program.cs
@@ -4,9 +4,21 @@ class Program
 {
     //static FileAttributes attrDir = FileAttributes.Directory;
     //static FileAttributes attrArchive = FileAttributes.Archive;
+
+    /// <summary>
+    ///     Returns if file is problematic
+    ///     as a side effect prints problematic files and folders to user (with some exceptions user is not informed about $RECYCLE.BIN etc)
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="depth"></param>
+    /// <returns></returns>
     static bool PrintIfProblematic(FileSystemInfo info, int depth)
     {
-        if (depth == 1 && info.Name == "Desktop.ini") // V hloubce 1 chci odpustit Desktop.ini slozky cisto, pc, atd ... maji ikonu
+        if (info.Name == "$RECYCLE.BIN")
+            return true;
+        if (info.Name == "System Volume Information")
+            return true;
+        if (depth == 1 && info.Name.ToLower() == "Desktop.ini") // V hloubce 1 chci odpustit Desktop.ini slozky cisto, pc, atd ... maji ikonu
             return false;
         if (info.Name == ".owncloudsync.log")
             return false;
@@ -18,11 +30,18 @@ class Program
             return false;
         if (info.Name == ".sync_journal.db-wal")
             return false;
+        if (info.Name[0] == '.' && info.Name.Contains("sync") && info.Name.Contains(".db"))
+            return false;
         if (info.Attributes.HasFlag(FileAttributes.ReparsePoint) && info.Name.EndsWith(".iso"))
             return false;
         if (info.FullName.Length > 250)
         {
             Console.WriteLine("Long: " + info.FullName);
+            return true;
+        }
+        if (info.Name[0] == '.')
+        {
+            Console.WriteLine("Starts with dot: " + info.FullName);
             return true;
         }
         FileAttributes attributes = info.Attributes;
@@ -50,7 +69,17 @@ class Program
     {
         string path;
         if (args.Length == 0)
-            args = new string[] { Directory.GetCurrentDirectory() };
+        {
+			Console.WriteLine("Usage: ListHiddenFiles folder [more folders]");
+			Console.WriteLine("Recursively searches the specified folders for file and folders with nonstandard permissions");
+            Console.WriteLine("It prints all problems found.");
+            Console.WriteLine("This app detects:");
+            Console.WriteLine(" - hidden files and folders (specfied by attribute in windows)");
+            Console.WriteLine(" - read only files and folders");
+            Console.WriteLine(" - other attributes");
+            Console.WriteLine(" - files with name starting with .");
+            Console.WriteLine(" - long paths");
+        }
         foreach (var arg in args)
         {
             DirectoryInfo dir = new DirectoryInfo(arg);
